7fdbefddee076b69c86cd6e069d436b9e7b1afa0
Dave Turner <davetii@gmail.com>
2017-02-07 21:31:34 -0500

    Player stuff

--- gametime-common/src/main/java/com/gametime/common/PlayerAttributes.java
+++ gametime-common/src/main/java/com/gametime/common/PlayerAttributes.java
@@ -4,4 +4,155 @@ package com.gametime.common;
  * Created by dave on 2/6/2017.
  */
 public class PlayerAttributes {
+
+    private int agility;
+    private int charisma;
+    private int desire;
+    private int ego;
+    private int endurance;
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+
+    public int getDesire() {
+        return desire;
+    }
+
+    public void setDesire(int desire) {
+        this.desire = desire;
+    }
+
+    public int getEgo() {
+        return ego;
+    }
+
+    public void setEgo(int ego) {
+        this.ego = ego;
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getHandle() {
+        return handle;
+    }
+
+    public void setHandle(int handle) {
+        this.handle = handle;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+
+    public int getPerson() {
+        return person;
+    }
+
+    public void setPerson(int person) {
+        this.person = person;
+    }
+
+    public int getShotSelection() {
+        return shotSelection;
+    }
+
+    public void setShotSelection(int shotSelection) {
+        this.shotSelection = shotSelection;
+    }
+
+    public int getShotSkill() {
+        return shotSkill;
+    }
+
+    public void setShotSkill(int shotSkill) {
+        this.shotSkill = shotSkill;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    private int energy;
+    private int handle;
+    private int health;
+    private int intelligence;
+    private int luck;
+    private int person;
+    private int shotSelection;
+    private int shotSkill;
+    private int size;
+    private int speed;
+    private int strength;
+
+
+
+
+
 }
--- gametime-common/src/main/java/com/gametime/common/PlayerSkillSet.java
+++ gametime-common/src/main/java/com/gametime/common/PlayerSkillSet.java
@@ -4,4 +4,101 @@ package com.gametime.common;
  * Created by dave on 2/6/2017.
  */
 public class PlayerSkillSet {
+
+    private final double acumen;
+    private final double ballProtection;
+    private final double defense;
+    private final double defenseRebound;
+    private final double drive;
+    private final double freeThrows;
+    private final double pressureDefense;
+    private final double longRange;
+    private final double offense;
+    private final double offenseRebound;
+    private final double passing;
+    private final double perimeter;
+    private final double post;
+    private final double teamDefense;
+
+    public PlayerSkillSet(PlayerAttributes attribs) {
+        acumen = 0;
+        ballProtection = 0;
+        defense = 0;
+        defenseRebound =0;
+        drive =0;
+        freeThrows = 0;
+        pressureDefense = 0;
+        longRange = 0;
+        offense = 0;
+        offenseRebound= 0;
+        passing = 0;
+        perimeter = 0;
+        post= 0;
+        teamDefense = 0;
+
+
+
+
+
+
+
+
+
+
+    }
+    public double getAcumen() {
+        return acumen;
+    }
+
+    public double getBallProtection() {
+        return ballProtection;
+    }
+
+    public double getDefense() {
+        return defense;
+    }
+
+    public double getDefenseRebound() {
+        return defenseRebound;
+    }
+
+    public double getDrive() {
+        return drive;
+    }
+
+    public double getFreeThrows() {
+        return freeThrows;
+    }
+
+    public double getPressureDefense() {
+        return pressureDefense;
+    }
+
+    public double getLongRange() {
+        return longRange;
+    }
+
+    public double getOffense() {
+        return offense;
+    }
+
+    public double getOffenseRebound() {
+        return offenseRebound;
+    }
+
+    public double getPassing() {
+        return passing;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getPost() {
+        return post;
+    }
+
+    public double getTeamDefense() {
+        return teamDefense;
+    }
 }
